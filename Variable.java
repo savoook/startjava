7dbccd5c69d34fce718b091d369047e1ee3e9371
Pavel Savin <Pavel.Savin@avon.com>
2021-05-25 19:00:11 +0300

    First HW

---
+++ Variable.java
@@ -0,0 +1,21 @@
+public class Variable{
+    public static void main(String[]args){
+        byte noize = 3;
+        short booferSize = 16;
+        int speed = 300;
+        long price =80000;
+        float weight = 5.343f;
+        double height = 1.2;
+        char model = 'G';
+        boolean actual = true;
+        System.out.println("noize: "+noize+" ,booferSize: "+booferSize+" ,speed: "+speed+" ,price: "+price+
+                        " ,weight: "+weight+" ,height: "+height+" ,model: "+model+" ,actual: "+actual);
+    }
+}
+
+
+
+
+
+
+
