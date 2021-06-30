e37dd9886cb0dd1ffe231a46400ecaead67b880b
Pavel Savin <Pavel.Savin@avon.com>
2021-06-30 13:36:55 +0300

    Lesson 2_HW3

--- Lesson_2/Wolf.java
+++ Lesson_2/Wolf.java
@@ -1,27 +1,72 @@
 public class Wolf {
-    String sex;
-    String name;
-    float weight;
-    int age;
-    String woolColor;
- 
-    void go() {
-        System.out.println(this.name + " пошел");
+    private String sex;
+    private String name;
+    private float weight;
+    private int age;
+    private String woolColor;
+
+    public void go() {
+        System.out.println(this.name + " go");
     }
 
-    void seat() {
-        System.out.println(this.name + " сел");
+    public void seat() {
+        System.out.println(this.name + " seat");
     }
 
-    void run() {
-        System.out.println(this.name + " побежал");
+    public void run() {
+        System.out.println(this.name + " run");
     }
 
-    void cry() {
-        System.out.println(this.name + " завыл");
+    public void cry() {
+        System.out.println(this.name + " cry");
     }
 
-    void hunt() {
-        System.out.println(this.name + " охотится");
+    public void hunt() {
+        System.out.println(this.name + " hunt");
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age > 8 && age <= 0) {
+            System.out.println("Incorrect age!");
+        } else {
+            this.age = age;
+        }
+    }
+
+    public String getWoolColor() {
+        return woolColor;
+    }
+
+    public void setWoolColor(String woolColor) {
+        this.woolColor = woolColor;
     }
 }
+
--- Lesson_2/WolfTest.java
+++ Lesson_2/WolfTest.java
@@ -2,11 +2,14 @@ public class WolfTest {
     public static void main(String[] args) {
         Wolf wolf1 = new Wolf();
         
-        wolf1.sex = "пацан";
-        wolf1.name = "Щаспою";
-        wolf1.weight = 20.5f;
-        wolf1.age = 12;
-        wolf1.woolColor = "серый";
+        wolf1.setSex("male");
+        wolf1.setName("Dude");
+        wolf1.setWeight(20.5f);
+        wolf1.setAge(12);
+        wolf1.setWoolColor("grey");
+
+        System.out.println("Wolf parametres: sex - " + wolf1.getSex() + ", name - " + 
+            wolf1.getName() + ", weight - " + wolf1.getWeight() + ", age - " + wolf1.getAge() + ", woolcolor - " + wolf1.getWoolColor());
 
         wolf1.go();
         wolf1.seat();
